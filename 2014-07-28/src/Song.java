806801536fb0b5a18cd5d597e0aa1d307368c810
Maurizio Minieri <mauminieri@gmail.com>
2022-12-28 16:01:17 +0100

    Update Song.java

--- 2014-07-28/src/Song.java
+++ 2014-07-28/src/Song.java
@@ -1,23 +1,24 @@
 import java.util.*;
+
 /*
- * . (Playlist, 2014-7-28)
+(Playlist, 2014-7-28)
 Implementare le classi Song e Playlist. Una canzone è caratterizzata dal nome e dalla durata in
-secondi. Una playlist è una lista di canzoni, compresi eventuali duplicati, ed ore il metodo add,
+secondi. Una playlist è una lista di canzoni, compresi eventuali duplicati, ed offre il metodo add,
 che aggiunge una canzone in coda alla lista, e remove, che rimuove tutte le occorrenze di una
-canzone dalla lista. Inne, la classe Playlist è dotata di ordinamento naturale basato sulla durata
+canzone dalla lista. Infine, la classe Playlist è dotata di ordinamento naturale basato sulla durata
 totale di ciascuna playlist.
 Sono preferibili le implementazioni in cui il confronto tra due playlist avvenga in tempo costante.
 Esempio d'uso:
-Song one = new Song("One", 275), two = new Song("Two", 362);
-Playlist a = new Playlist(), b = new Playlist();
-a.add(one); a.add(two); a.add(one);
-b.add(one); b.add(two);
-System.out.println(a.compareTo(b));
-a.remove(one);
-System.out.println(a.compareTo(b));
+	Song one = new Song("One", 275), two = new Song("Two", 362);
+	Playlist a = new Playlist(), b = new Playlist();
+	a.add(one); a.add(two); a.add(one);
+	b.add(one); b.add(two);
+	System.out.println(a.compareTo(b));
+	a.remove(one);
+	System.out.println(a.compareTo(b));
 Output:
-1
--1
+	1
+	-1
  */
 
 public class Song {
